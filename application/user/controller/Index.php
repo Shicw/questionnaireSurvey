5a5792df82c694243321c24480f95870557960b2
shicw <362559994@qq.com>
2019-03-06 10:58:56 +0800

    完成问卷作答提交功能

--- application/user/controller/Index.php
+++ application/user/controller/Index.php
@@ -3,6 +3,7 @@ namespace app\user\controller;
 
 use think\Controller;
 use think\Db;
+use think\Exception;
 
 class Index extends Controller
 {
@@ -30,5 +31,45 @@ class Index extends Controller
 
         $this->assign(['questionnaire'=>$questionnaire,'questions'=>$questions]);
         return $this->fetch();
+    }
+    /**
+     * 问卷作答提交
+     */
+    public function post(){
+        if(!$this->request->isPost()){
+            $this->error('请求失败');
+        }
+        $data = $this->request->post();
+        if(count($data)===1){
+            $this->error('不能提交空白问卷');
+        }
+        $ip = $this->request->ip();//ip地址
+        $questionnaireId = $data['questionnaireId'];//问卷id
+        $answer = $data['data'];//回答内容
+
+        //开启事务处理,将作答信息写入数据库
+        Db::startTrans();
+        try{
+        //循环每一个回答
+        foreach ($answer as $key => $a){
+            //获取当前的题号
+            $questionId = explode('-',$key)[1];
+            $result = Db::name('answer')->insert([
+                'questionnaire_id' => $questionnaireId,
+                'question_id' => $questionId,
+                'option_index' => $a,
+                'create_time' => time(),
+                'ip' => $ip
+            ]);
+            if(!$result){
+                throw new Exception("问卷提交失败,请重试");
+            }
+        }
+            Db::commit();// 提交事务
+        }catch(Exception $e){
+            Db::rollback();// 回滚事务
+            return ['code' => 0, 'msg' => $e->getMessage()];
+        }
+        return ['code' => 1, 'msg' =>'问卷提交成功!'];
     }
 }
